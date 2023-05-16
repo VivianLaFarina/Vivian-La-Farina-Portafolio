d9c81321da53299ca5bff000c2dbc9680b808f04
Vivian la Farina <vivianlafarina@gmail.com>
2023-05-16 18:28:37 -0500

    :art:

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App font-['Bruno_Ace']">
+      <h3 className='text font-bold text-zinc-700 text-7xl'>Vivian La Farina </h3>
+
+    </div>
+  )
+}
+
+export default App
