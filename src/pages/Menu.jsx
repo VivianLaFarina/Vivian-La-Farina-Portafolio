90bc167c69239fc69a7a713c08c4f1290aff1cae
Vivian La Farina <vivianlafarina@gmail.com>
2023-07-24 09:15:09 -0500

    menu zide

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -10,19 +10,19 @@ const Menu = () => {
       <Header />
 
       {/* Introduction section */}
-      <section className='flex justify-center py-10 px-4 text-4xl md:text-6xl'>
+      <section className='flex justify-center py-10 px-4 text-2xl md:text-4xl'>
         <table className="table p-4 bg-white rounded-lg shadow ">
           <thead>
             <tr>
-              <th className="border-b-2 p-4 text-3xl text-slate-600 dark:border-dark-5 whitespace-nowrap font-normal"></th>
-              <th className="border-b-2 p-4 text-5xl text-slate-600 dark:border-dark-5 whitespace-nowrap font-normal">
+              <th className="border-b-2 p-4 text-2xl text-slate-600 dark:border-dark-5 whitespace-nowrap font-normal"></th>
+              <th className="border-b-2 p-4 text-3xl text-slate-600 dark:border-dark-5 whitespace-nowrap font-normal">
                 Menu
               </th>
             </tr>
           </thead>
           <tbody>
             <tr className="text-gray-600">
-              <td className="border-b-2 p-4 dark:border-dark-5 text-slate-500 text-3xl">
+              <td className="border-b-2 p-4 dark:border-dark-5 text-slate-500 text-2xl">
                 <i className='bx bx-terminal'></i>
               </td>
               <td className="border-b-2 p-4 dark:border-dark-5">
@@ -34,7 +34,7 @@ const Menu = () => {
             </tr>
             <tr className="text-gray-600">
               <td className="border-b-2 p-4 dark:border-dark-5">
-                <i className='bx bx-briefcase-alt text-slate-500 text-3xl'></i>
+                <i className='bx bx-briefcase-alt text-slate-500 text-2xl'></i>
               </td>
               <td className="border-b-2 p-4 dark:border-dark-5">
                 <Link to="/projects" className='font-["Bruno_Ace"]'>
@@ -45,7 +45,7 @@ const Menu = () => {
             </tr>
             <tr className="text-gray-600">
               <td className="border-b-2 p-4 dark:border-dark-5">
-                <i className='bx bx-info-circle text-slate-500 text-3xl'></i>
+                <i className='bx bx-info-circle text-slate-500 text-2xl'></i>
               </td>
               <td className="border-b-2 p-4 dark:border-dark-5">
                 <Link to="/about" className='font-["Bruno_Ace"]'>
@@ -56,7 +56,7 @@ const Menu = () => {
             </tr>
             <tr className="text-gray-600">
               <td className="border-b-2 p-4 dark:border-dark-5">
-                <i className='bx bx-conversation text-slate-500 text-3xl'></i>
+                <i className='bx bx-conversation text-slate-500 text-2xl'></i>
               </td>
               <td className="border-b-2 p-4 dark:border-dark-5">
                 <Link to="/contact" className='font-["Bruno_Ace"]'>
